50a7f3db43a571ec77dfc19504dbd0c18768f2d0
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-06-03 23:14:49 +0200

    Update Lcd_test.c
    test_lcd

--- Lcd_test.c
+++ Lcd_test.c
@@ -1,1 +1,26 @@
 
+#include"tm4c123gh6pm.h"
+#include "stdint.h"
+#include "stdio.h"
+void SystemInit(){}
+char str[3];
+	float dis=100;
+int main()
+{int i = 0;
+	SysTick_Init();
+	LCD_init();
+	sprintf(str,"%f",dis);
+	LCD_sendCommand(1); /* clear display */
+LCD_sendCommand(0x80); /* LCD cursor location */
+		
+	while(str[i] != '\0')
+	{
+		LCD_displayCharacter(str[i]);
+		i++;
+	}
+		SysTick_Wait1ms(500);
+while(1)
+{
+
+}
+}
