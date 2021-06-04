e6052523dceba3041c36ea5608c57be0db777691
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-06-04 20:24:11 +0200

    Update Lcd_test.c

--- Lcd_test.c
+++ Lcd_test.c
@@ -2,6 +2,7 @@
 #include"tm4c123gh6pm.h"
 #include "stdint.h"
 #include "stdio.h"
+#include "Lcd.h"
 void SystemInit(){}
 
 char str[3];
@@ -10,18 +11,18 @@ float dis=100;
 int main()
 {
 	int i = 0;
-	SysTick_Init();
-	LCD_init();
-	sprintf(str,"%f",dis);
+	SysTick_Init(); //initiate systick for delays
+	LCD_init();  //initiate lcd 
+	sprintf(str,"%f",dis);   //convert float to string 
 	LCD_sendCommand(1); /* clear display */
-  LCD_sendCommand(0x80); /* LCD cursor location */
+        LCD_sendCommand(0x80); /* LCD cursor location */
 		
-	while(str[i] != '\0')
+	while(str[i] != '\0')// loop to diplay characters
 	{
-		LCD_displayCharacter(str[i]);
+		LCD_displayCharacter(str[i]);//diplay character
 		i++;
 	}
-		SysTick_Wait1ms(500);
+		SysTick_Wait1ms(500);//delay 500ms
 while(1)
 {
 
