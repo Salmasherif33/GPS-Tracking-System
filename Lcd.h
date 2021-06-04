d5c39cb84fedd5d9c91fd2e4386ceee705f7cdc0
shimaa88 <73905180+shimaa88@users.noreply.github.com>
2021-06-04 13:23:19 +0200

    Update Lcd.h

--- Lcd.h
+++ Lcd.h
@@ -1,1 +1,22 @@
-
+ /******************************************************************************
+ *
+ * Module: LCD
+ *
+ * File Name: lcd.h
+ *
+ * Description: Header file for the LCD driver
+ *
+ *
+ *******************************************************************************/
+ #ifndef LCD_H_
+#define LCD_H_
+#include"tm4c123gh6pm.h"
+#include "stdint.h"
+#include "stdio.h"
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/ 
+ 
+void LCD_init(void);
+void LCD_displayString(const char *Str);
+void LCD_sendCommand(uint8_t command);
