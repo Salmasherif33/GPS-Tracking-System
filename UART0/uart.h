c27408cb64d53156391e9856fdddc9a8a88d541d
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-06-05 16:35:00 +0200

    collecting them in one folder

---
+++ UART0/uart.h
@@ -0,0 +1,17 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+#include "stdint.h"
+#include "C:/Keil/EE319Kware/inc/tm4c123gh6pm.h"
+
+void UART0_Init(void);
+
+
+uint8_t UART0_Available(void);
+uint8_t UART0_Read(void);
+void uart0_write(uint8_t data);
+
+
+
+
+#endif //__UART_H__
