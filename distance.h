8f27f4121a7b42f4d75cdd3a955ac093db4554b2
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-06-05 04:37:19 +0200

    Delete distance.h

--- distance.h
+++
@@ -1,9 +0,0 @@
-#ifndef __distance_H__
-#define __distance_H__
-
-
-
-double calc_dist_bet_2_points(double p1Lat, double p2Lat,double p1Lang,double p2Lang);
-void conv (char finalvalue[7],double dis);
-
-#endif 
