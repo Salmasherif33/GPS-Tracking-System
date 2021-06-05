ef5c370ab01c9d0c396853c10580d51a61139a55
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-06-05 16:37:51 +0200

    adding comments

--- UART0/uart.h
+++ UART0/uart.h
@@ -4,9 +4,9 @@
 #include "stdint.h"
 #include "C:/Keil/EE319Kware/inc/tm4c123gh6pm.h"
 
+//proto types for the functions
+
 void UART0_Init(void);
-
-
 uint8_t UART0_Available(void);
 uint8_t UART0_Read(void);
 void uart0_write(uint8_t data);
