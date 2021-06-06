0ec392270884229d266b719f43e4053bee205293
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-06-06 22:08:08 +0200

    Update distance.c

--- distance/distance.c
+++ distance/distance.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 
+//calculate distance
 
 float rad (float x){
     return x * PI / 180;
@@ -14,18 +15,18 @@ float calc_dist_bet_2_points(float p1Lat, float p2Lat,float p1Lang,float p2Lang)
    float dLat = rad(p2Lat - p1Lat);
    float dLong = rad(p2Lang - p1Lang);
    float a = sin(dLat / 2) * sin(dLat / 2)+
-    cos(rad(p1Lat)) * cos(rad(p2Lat))*
-    sin(dLong / 2) * sin(dLong / 2);
-
-    float c = 2 * atan2(sqrt(a), sqrt(1 - a));
-    float  d = R * c;
-return d;
-	 
+   cos(rad(p1Lat)) * cos(rad(p2Lat))*
+   sin(dLong / 2) * sin(dLong / 2);
+   float c = 2 * atan2(sqrt(a), sqrt(1 - a));
+   float  d = R * c;
+   return d;	 
 }
+
+//converting from char to float
 
  void conv (char finalvalue[7],float dis)
 {
-				sprintf(finalvalue,"%f",dis);
+   sprintf(finalvalue,"%f",dis);
 }
 
 	
