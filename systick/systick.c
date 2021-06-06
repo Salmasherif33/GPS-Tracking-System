a743c66600184ffc646e5a2558573dc57ac62e8b
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-06-06 22:12:50 +0200

    Update systick.c

--- systick/systick.c
+++ systick/systick.c
@@ -1,4 +1,6 @@
 #include "systick.h"
+//initializing systic timer
+
 void SysTick_Init(void){
 NVIC_ST_CTRL_R = 0; // 1) disable SysTick during setup
 NVIC_ST_RELOAD_R = 0x00FFFFFF; // 2) maximum reload value
@@ -15,22 +17,24 @@ NVIC_ST_RELOAD_R = 16-1; // number of counts
 NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
 while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for flag
 }}
-	void SysTick_Wait1ms(uint32_t delay)
+// wait 1ms
+
+void SysTick_Wait1ms(uint32_t delay)
+{
+        uint32_t i;
+	for(i=0;i<delay;i++)
 	{
-	uint32_t i;
-		for(i=0;i<delay;i++)
-		{
-		SysTick_Wait();
-		
-		}
-	}
-		void SysTick_Wait1us(uint32_t delay)
-	{
-	uint32_t i;
-		for(i=0;i<delay;i++)
-		{
-		SysTick_Waitu();
-		
-		}
+	  SysTick_Wait();
 
 	}
+}
+//wait 1 micro sec
+
+void SysTick_Wait1us(uint32_t delay)
+{
+        uint32_t i;
+	for(i=0;i<delay;i++)
+	{
+	SysTick_Waitu();
+	}
+}
