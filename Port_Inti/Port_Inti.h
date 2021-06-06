c3b5ad8101c93cbf44521c1fe931eab53c093b40
iamRenadMostafa <1700553@eng.asu.edu.eg>
2021-06-06 18:02:16 +0200

    Update_port_inti

--- Port_Inti/Port_Inti.h
+++ Port_Inti/Port_Inti.h
@@ -39,3 +39,27 @@ void Port_B(void)
   GPIO_PORTB_DEN_R   |= 0xFF;         /* Enable Digital I/O onPORTB */
   GPIO_PORTB_DATA_R   &= 0x0;         /* Clear data in PORT B */
 	}
+	
+void LCD_init(void)
+ {SYSCTL_RCGCGPIO_R |= 0x02; /* activate Port B */
+  while((SYSCTL_PRGPIO_R&=0x00000002) == 0){};/*waite to activate*/ 
+  GPIO_PORTB_AMSEL_R &= 0;         /* Disable Analog on PORTB*/
+  GPIO_PORTB_PCTL_R   &= 0x00000000;   /* Clear PMCx bits for  PORTB to use it as GPIO pin */
+  GPIO_PORTB_DIR_R    |= 0xFF;         /* Configure PORTB as output pin */
+  GPIO_PORTB_AFSEL_R &= 0x0;         /* Disable alternative function on PORTB */
+  GPIO_PORTB_DEN_R   |= 0xFF;         /* Enable Digital I/O onPORTB */
+  GPIO_PORTB_DATA_R   &= 0x0;         /* Clear data in PORT B */
+  SysTick_Wait1ms(20);//delay 20ms
+LCD_sendCommand(0X30);//wakeup
+SysTick_Wait1ms(5);//DELAY 5mS
+LCD_sendCommand(0X30);//wakeup
+SysTick_Wait1us(100);//DELAY 40uS
+LCD_sendCommand(0X30);//wakeup
+SysTick_Wait1us(40);//DELAY 40uS
+LCD_sendCommand(0x20); /* use 4-bit data mode */
+SysTick_Wait1us(40);//DELAY 40uS
+ LCD_sendCommand(0X28); /* Select 4-bit Mode of LCD */
+ LCD_sendCommand(0X06); /* clear whatever is written on display */
+ LCD_sendCommand(0X01); /* shift cursor right */
+ LCD_sendCommand(0X0F);  /* Enable Display and cursor blinking */
+ }
