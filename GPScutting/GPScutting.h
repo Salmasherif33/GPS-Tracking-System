b9ecc775bb64cfa94db301946fa52d59b9394067
Rawan Khaled <67297151+rere-kh1999@users.noreply.github.com>
2021-06-06 22:17:57 +0200

    Update GPScutting.h

--- GPScutting/GPScutting.h
+++ GPScutting/GPScutting.h
@@ -1,8 +1,14 @@
 #ifndef __GPScutting_H__
 #define __GPScutting_H__
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
 
 
-
-void GPSModule(void);
+void GPSModule(char latitude []);
 
 #endif
