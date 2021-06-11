09e3d0659fbc32a39a6965e6dc4a4eb2227cde57
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-06-11 16:15:40 +0200

    Update distance.h

--- distance/distance.h
+++ distance/distance.h
@@ -1,7 +1,8 @@
 #ifndef __distance_H__
 #define __distance_H__
 
-
+#include <math.h>
+#include <stdio.h>
 
 float calc_dist_bet_2_points(float p1Lat, float p2Lat,float p1Lang,float p2Lang);
 void conv (char finalvalue[7],float dis);
