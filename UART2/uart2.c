3354b87d8415d759ee9d0f8df73f5e36eea61ba8
Nouran Ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2021-06-11 20:26:52 +0200

    Update uart2.c

--- UART2/uart2.c
+++ UART2/uart2.c
@@ -4,17 +4,25 @@
 
 void UART2_Init()
 {
-//clock to port D
+	
+	
+        //clock to port D
 	
 	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
-  while((SYSCTL_PRGPIO_R & 0x00000008) == 0){};
+	
+	//wait for the clock to be enabled to port D 
+	
+        while((SYSCTL_PRGPIO_R & 0x00000008) == 0){};
+	
 	//clock to uart2 at port D
 	
 	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R2;
-	GPIO_PORTD_LOCK_R=GPIO_LOCK_KEY;
-
 	
-	//clear enable (disable) uart0
+	//remove port D lock
+	
+	GPIO_PORTD_LOCK_R=GPIO_LOCK_KEY;
+	
+	//clear enable (disable) uart2
 	
 	UART2_CTL_R &= ~UART_CTL_UARTEN;
 	
@@ -28,16 +36,19 @@ void UART2_Init()
 	
 	UART2_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
 	
-	//enable uart1,TX,RX
+	//enable uart2 , TX , RX
 	
 	UART2_CTL_R |= (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN);
 	
+	//allow changes to this bits
+	
 	GPIO_PORTD_CR_R = 0xFF;
-	//select UART instead of GPIO
+	
+	//select alternate function instead of GPIO
 	
 	GPIO_PORTD_AFSEL_R = GPIO_PD10_M;	//011000000
 	
-	// select uart alternate function
+	// select uart2 alternate function
 	
 	GPIO_PORTD_PCTL_R = ( GPIO_PORTD_PCTL_R  & 0x00FFFFFF ) |  (GPIO_PCTL_PD7_U2TX | GPIO_PCTL_PD6_U2RX);
 	
@@ -45,17 +56,18 @@ void UART2_Init()
 	
 	GPIO_PORTD_DEN_R = GPIO_PD10_M;
 	
-	//analog disable
 	
-	//GPIO_PORTA_AMSEL_R &= ~0x03;	
 	
 }
 
 uint8_t UART2_Available(void){
+	
 	return ((UART2_FR_R&UART_FR_RXFE) == UART_FR_RXFE) ? 0 : 1;
+	
 }
 
 // to read data from UART ( latitude and longitude )s
+
 uint8_t UART2_Read(void)
 {
 
