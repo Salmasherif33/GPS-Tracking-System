5f5e24f4f1c1dfb7331bea23a816cf92fcb420fa
Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
2021-06-11 16:22:11 +0200

    Update systick.h

--- systick/systick.h
+++ systick/systick.h
@@ -3,9 +3,9 @@
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
 void SysTick_Init(void);
-void SysTick_Wait();
+void SysTick_Wait(void);
 void SysTick_Wait1ms(uint32_t delay);
 void SysTick_Wait1us(uint32_t delay);
-void SysTick_Waitu();
+void SysTick_Waitu(void);
 
 #endif 
